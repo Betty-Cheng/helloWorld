0bad43169628731ceb60fc32295d4e6e4f8314ef
Betty-Cheng <wishfulbetty@gmail.com>
2021-08-12 14:15:33 -0700

    completed variable workshop

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// = is an assignment
+var firstName = "Betty";
+let lastName = "Cheng";
+//cannot reassign const
+const age = 18;
+//variable reassignments
+firstName = "Kage";
+lastName = "Hina";
+
+console.log('Hello ' + firstName + ' ' + lastName + '!'
++ ' You are ' + age + ' years old!');
